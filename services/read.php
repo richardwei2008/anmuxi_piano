77689aa5246fc02dd2122272cdaa15f719aed382
Richard <weizhe_2008@aliyun.com>
2014-06-27 08:32:22 +0800

    openId pass through and my rank

--- services/read.php
+++ services/read.php
@@ -3,9 +3,11 @@
 //需要执行的SQL语句
 //单条
 $openId = "openid1";
-$openId = $_SESSION['openId'];
+$openId = $_GET['openId'];
 $sql="SELECT id, openId, nickname FROM user_info LIMIT 6";
 $sql_me="SELECT info.id, info.openId, nickname, info.headimgurl, info.score FROM user_info info where info.openId = '$openId'";
+// $sql_rank="SELECT rank, id, openid, nickname, headimgurl, score FROM (SELECT (@rowNum:=@rowNum+1) rank, id, openid, nickname, headimgurl, score FROM user_info,(SELECT (@rowNum :=0) ) b order by score desc)t WHERE t.openid='oxqECj7JTrpVG7BfJnNCUpQap0Xc'";
+$sql_rank="SELECT rank, id, openid, nickname, headimgurl, score FROM (SELECT (@rowNum:=@rowNum+1) rank, id, openid, nickname, headimgurl, score FROM user_info,(SELECT (@rowNum :=0) ) b order by score desc)t WHERE t.openid='$openId'";
 $sql_global="SELECT info.id, info.openId, nickname, info.headimgurl, info.score FROM user_info info ORDER BY info.score desc LIMIT 6";
 $sql_total="SELECT count(info.id) as value FROM user_info info ";
 //多条数据
@@ -17,7 +19,7 @@ require('conn.php');
 
 
 //执行SQL语句(查询) 
-$result_me = mysql_query($sql_me) or die('数据库查询失败！</br>错误原因：'.mysql_error()); 
+$result_me = mysql_query($sql_rank) or die('数据库查询失败！</br>错误原因：'.mysql_error()); 
 $result_global = mysql_query($sql_global) or die('数据库查询失败！</br>错误原因：'.mysql_error()); 
 $result_total= mysql_query($sql_total) or die('数据库查询失败！</br>错误原因：'.mysql_error()); 
 //提示操作成功信息，注意：$result存在于conn.php文件中，被调用出来 
--- services/save.php
+++ services/save.php
@@ -13,26 +13,31 @@ $province = $jsonPOST->province;
 $country = $jsonPOST->country;
 $headimgurl = $jsonPOST->headimgurl;
 $subscribe_time = $jsonPOST->subscribe_time;
-$sql_query = "SELECT count(info.id) as value FROM user_info info WHERE openid = '$openId'";
+$sql_query = "SELECT info.id, info.openId, nickname, info.headimgurl, info.score FROM user_info info WHERE openid = '$openId'";
 $sql_insert = "INSERT INTO user_info (score, subscribe, openid, nickname, sex, language, city, province, country, headimgurl, subscribe_time) VALUES ($score, '$subscribe', '$openId', '$nickname', $sex, '$language', '$city', '$province', '$country', '$headimgurl', $subscribe_time)";
 $sql_update = "UPDATE user_info set score = $score, subscribe = '$subscribe', nickname = '$nickname', sex=$sex, language='$language', city='$city', province='$province', country='$country', headimgurl='$headimgurl', subscribe_time=$subscribe_time WHERE openid = '$openId'";
 
 $result = mysqli_query($mysqli_con, $sql_query);
-$count = mysqli_fetch_object($result);
-// echo json_encode(array('count'=>$count));
-if ($count->value == 0 || $openId == "" || $openId == null) {
-	if (mysqli_query($mysqli_con, $sql_insert)) {
-		echo json_encode(array('user'=>$jsonPOST, 'message'=>"Successfully inserted ". mysqli_affected_rows($mysqli_con)."row"));
-	} else {
-		echo "Error occurred: " . mysqli_error($mysqli_con);
+$me = mysqli_fetch_object($result);
+echo json_encode(array('me'=>$me, 'openid' => $openId, 'sql'=>$sql_query));
+if (null != $openId && "" != $openId) {
+	if ($me == null) {
+		if (mysqli_query($mysqli_con, $sql_insert)) {
+			echo json_encode(array('user'=>$jsonPOST, 'me'=>$me, 'message'=>"Successfully inserted ". mysqli_affected_rows($mysqli_con)."row"));
+		} else {
+			echo "Error occurred: " . mysqli_error($mysqli_con);
+		}
+	} else if ($me->score <= $score) {
+		if (mysqli_query($mysqli_con, $sql_update)) {
+			echo json_encode(array('user'=>$jsonPOST, 'me'=>$me, 'message'=>"Successfully updated ". mysqli_affected_rows($mysqli_con)."row"));
+		} else {
+			echo "Error occurred: " . mysqli_error($mysqli_con);
+		}
 	}
-} else if ($openId != null && $openId != "") {
-	if (mysqli_query($mysqli_con, $sql_update)) {
-		echo json_encode(array('user'=>$jsonPOST, 'message'=>"Successfully updated ". mysqli_affected_rows($mysqli_con)."row"));
-	} else {
-		echo "Error occurred: " . mysqli_error($mysqli_con);
-	}
+} else {
+	echo json_encode(array('user'=>$jsonPOST, 'me'=>$me, 'message'=>"Invalid Open Id [".$openId."]"));
 }
+
 
 mysqli_close($mysqli_con);
 ?>
